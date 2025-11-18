65ba53d50562269cbad8ee7707f56a150be8f1df
bizhym0512 <bizhym0512@gmail.com>
2025-11-18 21:20:47 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: false,   // ← これを追加（重要！！）
+  },
 };
 
 module.exports = nextConfig;
